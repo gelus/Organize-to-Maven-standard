18e34e5f11ba1dcedbf4dc39d65393599980d664
Gelus <benjarmil@gmail.com>
2013-10-23 11:22:39 -0600

    no example for you

--- organize/config.js
+++ organize/config.js
@@ -2,8 +2,11 @@
 	// It is an object with each language directory defined as a key.
 	// the value of these propertys will be objects 
 	// that may or may not hold an exclude array or regExp values
-	// eg config={"js": {exclude: [/min.js$/]}}
-module.exports = {
-	"js": {exclude: [/min.js$/]},
-	"less": {}
-}
+	// eg
+
+	// module.exports = {
+	// 	"js": {exclude: [/min.js$/]},
+	// 	"less": {}
+	// }
+
+	module.exports = {}
